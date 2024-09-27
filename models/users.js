c1de6c9f7287597bd4eee687302784b520351bf9
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-09-27 10:09:09 +0100

    added authentication

--- models/users.js
+++ models/users.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose")
+require("dotenv").config()
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const userSchema = new mongoose.Schema({
     Name:{
@@ -14,9 +17,32 @@ const userSchema = new mongoose.Schema({
         required:[true,"please enter an email"],
         match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "please enter a valid email"],
         unique:true
+    },
+
+    Password: {
+        type:String,
+        required:[true,"please enter a password"],
+        minlength:[7,"paswword must be more than seven characters"]
     }
 
 },
 {timestamps:true})
+
+userSchema.pre("save", async function(next){
+    const salt = await bcrypt.genSalt(10)
+    this.Password = await bcrypt.hash(this.Password,salt)
+    next()
+})
+
+userSchema.methods.createJWT = function() {
+    const jwtToken = jwt.sign({userId:this._id,name:this.Name},process.env.JWTT,{expiresIn:"30d"})
+    console.log(`your jwt token is ${jwtToken}`)
+    return jwtToken
+}
+
+userSchema.methods.comparePassword = function(passwordToCheck) {
+const isMatch = bcrypt.compare(passwordToCheck,this.Password)
+return isMatch
+}
 
 module.exports = mongoose.model("users",userSchema)
