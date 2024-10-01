7d33e4e09ff58f40214bc3fe65fed3bf1b0a4d11
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-10-01 15:04:47 +0100

    admin login section done

--- controllers/adminAuth.js
+++ controllers/adminAuth.js
@@ -1,8 +1,41 @@
 const User = require("../models/users")
 const statusCodes = require("http-status-codes")
 
-const adminLogin = (req,res)=> {
+const adminLogin = async (req,res)=> {
 //res.send("admin Login")
+
+const {email,password} = req.body
+
+if(!email) {
+    return res.status(statusCodes.BAD_REQUEST).json({msg:"please enter your email"})
+}
+else if(!password) {
+    return res.status(statusCodes.BAD_REQUEST).json({msg:"please enter your password"})
+}
+
+try {
+    const _user = await User.findOne({email:email})
+    
+    if(!_user) {
+      return  res.status(statusCodes.NOT_FOUND).json({msg:`No User with ${email} found`})
+    }
+
+    const isPassword = await _user.comparePassword(password)
+
+    if(!isPassword) {
+        return res.status(statusCodes.UNAUTHORIZED).json({msg:"AUTHENTICATION FAILED"})
+    }
+
+    const jwtToken = await _user.createJWT()
+
+    res.status(statusCodes.OK).json({
+        user:{name:_user.name},
+        token:jwtToken
+    })
+    
+}catch(error){
+    res.status(statusCodes.INTERNAL_SERVER_ERROR).json({msg:error})
+}
 }
 
 
