23dadc21f2698bcaee6374b974e65cf24a2dd1d1
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-10-03 00:42:20 +0100

    like and unlike comment works

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const blog = require("../models/blogModel");
 const statusCodes = require("http-status-codes");
-const comments = require("../models/comment")
-const { object } = require("joi");
+const comments = require("../models/comment");
 
 // Get all blogs
 const getAllBlogs = async (req, res) => {
@@ -18,7 +17,7 @@ const getAllBlogs = async (req, res) => {
 
     res.status(statusCodes.OK).json({ blogs: blogsWithLikesCount, numberOfBlogs });
   } catch (error) {
-    res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to retrieve blogs", error });
+    return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to retrieve blogs", error });
   }
 };
 
@@ -28,17 +27,17 @@ const getOneBlog = async (req, res) => {
   try {
     const blogContent = await blog.findOne({ _id: req.params.id }).populate("comments").populate("blogLikes","name");
 
-
     if (!blogContent) {
       return res.status(statusCodes.NOT_FOUND).json({ msg: "No content found" });
     }
-    blogContent.clicks++
-   await blogContent.save()
 
-   const numberOfLikes = blogContent.blogLikes.length;
-    res.status(statusCodes.OK).json({ blogContent,numberOfLikes });
+    blogContent.clicks++;
+    await blogContent.save();
+
+    const numberOfLikes = blogContent.blogLikes.length;
+    return res.status(statusCodes.OK).json({ blogContent, numberOfLikes });
   } catch (error) {
-    res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to retrieve the blog", error });
+    return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to retrieve the blog", error });
   }
 };
 
@@ -56,106 +55,122 @@ const likeABlog = async (req, res) => {
       return res.status(statusCodes.NOT_FOUND).json({ msg: "No content found" });
     }
 
-    
-    let objectIdUserId;
-    try {
-      objectIdUserId = mongoose.Types.ObjectId(userId);
-    } catch (error) {
+    if (!mongoose.isValidObjectId(userId)) {
       return res.status(statusCodes.BAD_REQUEST).json({ msg: "Invalid user ID" });
     }
 
+    let objectIdUserId = mongoose.Types.ObjectId(userId);
+
     // Check if the user has already liked the blog
-   if (blogContent.blogLikes.includes(objectIdUserId)) {
-    return res.status(statusCodes.BAD_REQUEST).json({msg:"This blog has already been liked"})
-   }
+    if (blogContent.blogLikes.includes(objectIdUserId)) {
+      return res.status(statusCodes.BAD_REQUEST).json({ msg: "This blog has already been liked" });
+    }
 
     // Add userId to the likes array
     blogContent.blogLikes.push(objectIdUserId);
     const numberOfLikes = blogContent.blogLikes.length;
     await blogContent.save();
 
-    res.status(statusCodes.OK).json({ blogContent, numberOfLikes });
+    return res.status(statusCodes.OK).json({ blogContent, numberOfLikes });
   } catch (error) {
-    res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to like the blog", error });
+    return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to like the blog", error });
   }
 };
 
 // Unlike a blog
 const unlikeABlog = async (req, res) => {
+  const {
+    user: { userId },
+    params: { id: blogId }
+  } = req;
 
+  try {
+    const blogContent = await blog.findOne({ _id: blogId });
 
-    const {
-      user: {userId},
-      params:{id:blogId}
-    } = req
-
-    try {
-      
-      const blogContent = await blog.findOne({_id:blogId})
-
-      if(!blogContent) {
-       return  res.status(statusCodes.NOT_FOUND).json({msg:`no Blog with id ${blogId} found`})
-      }
-
-      let objectUserID
-      try {
-       objectUserID = mongoose.Types.ObjectId(userId)
-      }catch(error){
-        res.status(statusCodes.BAD_REQUEST).json({msg:error})
-      }
-     
-        if(blogContent.blogLikes.includes(objectUserID)) {
-
-          blogContent.blogLikes = blogContent.blogLikes.filter((likes)=> likes.toString() !== objectUserID.toString())
-          const numberOfLikes = blogContent.blogLikes.length
-          await blogContent.save()
-          return res.status(statusCodes.OK).json({ msg: "Blog unliked", blogContent, numberOfLikes });
-
-        }else {
-          return res.status(statusCodes.BAD_REQUEST).json({msg:"this blog hasnt been liked"})
-        }
-      
-    }catch(error){
-      res.status(statusCodes.INTERNAL_SERVER_ERROR).json({msg:error})
+    if (!blogContent) {
+      return res.status(statusCodes.NOT_FOUND).json({ msg: `No Blog with id ${blogId} found` });
     }
+
+    if (!mongoose.isValidObjectId(userId)) {
+      return res.status(statusCodes.BAD_REQUEST).json({ msg: "Invalid user ID" });
+    }
+
+    let objectUserId = mongoose.Types.ObjectId(userId);
+
+    if (blogContent.blogLikes.includes(objectUserId)) {
+      blogContent.blogLikes = blogContent.blogLikes.filter((likes) => likes.toString() !== objectUserId.toString());
+      const numberOfLikes = blogContent.blogLikes.length;
+      await blogContent.save();
+      return res.status(statusCodes.OK).json({ msg: "Blog unliked", blogContent, numberOfLikes });
+    } else {
+      return res.status(statusCodes.BAD_REQUEST).json({ msg: "This blog hasn't been liked" });
+    }
+  } catch (error) {
+    return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to unlike the blog", error });
+  }
 };
 
 // Comment on a blog
 const commentOnABlog = async (req, res) => {
- try {
-  const{
-    user:{userId,name},
-    params:{id:blogId}
-  } = req
-
-  let objectUserId 
   try {
-    objectUserId = mongoose.Types.ObjectId(userId)
-  
-  }catch(error){
-    res.status(statusCodes.BAD_REQUEST).json({msg:error})
+    const {
+      user: { userId, name },
+      params: { id: blogId }
+    } = req;
+
+    if (!mongoose.isValidObjectId(userId)) {
+      return res.status(statusCodes.BAD_REQUEST).json({ msg: "Invalid user ID" });
+    }
+
+    req.body.UserName = name;
+    req.body.User = mongoose.Types.ObjectId(userId);
+
+    const commentContent = await comments.create(req.body);
+    const blogContent = await blog.findOne({ _id: blogId });
+
+    if (!blogContent) {
+      return res.status(statusCodes.BAD_REQUEST).json({ msg: `No blog with id ${blogId} found` });
+    }
+
+    blogContent.comments.push(commentContent._id);
+    await blogContent.save();
+
+    return res.status(statusCodes.OK).json({ commentContent, blogContent });
+  } catch (error) {
+    return res.status(statusCodes.BAD_REQUEST).json({ msg: error.message });
   }
-req.body.UserName = name
-req.body.User = objectUserId
-  const commentContent = await comments.create(req.body)
-  const blogContent = await blog.findOne({_id:blogId})
+};
 
-  if(!blogContent) {
-   return res.status(statusCodes.BAD_REQUEST).json({msg: `No blog with id ${blogId} found`})
+const likeUnlikeABlogComment = async (req, res) => {
+  const { user: { userId }, params: { commentId } } = req;
+
+  try {
+    const commentContent = await comments.findOne({ _id: req.params.id });
+
+    if (!commentContent) {
+      return res.status(statusCodes.BAD_REQUEST).json({ msg: "No comment found" });
+    }
+
+    if (!mongoose.isValidObjectId(userId)) {
+      return res.status(statusCodes.BAD_REQUEST).json({ msg: "Invalid user ID" });
+    }
+
+    let objectUserId = mongoose.Types.ObjectId(userId);
+
+    if (commentContent.likes.includes(objectUserId)) {
+      commentContent.likes = commentContent.likes.filter((like) => like.toString() !== objectUserId.toString());
+      const numberOfLikes = commentContent.likes.length;
+      await commentContent.save();
+      return res.status(statusCodes.OK).json({ msg: "Comment unliked successfully", numberOfLikes });
+    } else {
+      commentContent.likes.push(objectUserId);
+      const numberOfLikes = commentContent.likes.length;
+      await commentContent.save();
+      return res.status(statusCodes.OK).json({ msg: "Comment liked successfully", numberOfLikes });
+    }
+  } catch (error) {
+    return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ msg: error.message });
   }
+};
 
-   blogContent.comments.push(commentContent._id)
-   await blogContent.save()
- 
-  res.status(statusCodes.OK).json({commentContent,blogContent})
-
- }catch(error){
-  res.status(statusCodes.BAD_REQUEST).json({msg:error})
- }
-}
-
-const likeABlogComment = async(req,res)=> {
-  res.send("blog liked")
-}
-
-module.exports = { getAllBlogs, getOneBlog, likeABlog, unlikeABlog, commentOnABlog, likeABlogComment };
+module.exports = { getAllBlogs, getOneBlog, likeABlog, unlikeABlog, commentOnABlog, likeUnlikeABlogComment };
