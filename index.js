45f73091a09bb55a0e6408abdd2424730125d071
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-10-03 21:53:20 +0100

    connected to frontend

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const express = require("express")
-const connectDB = require("./db/connect")
-require("dotenv").config()
-const app = express()
+const express = require("express");
+const path = require("path"); // Import the 'path' module
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const app = express();
 
-const blogsRoute = require("./routes/blog")
-const adminRoute = require("./routes/adminRoute")
-const authRoute = require("./routes/auth")
-const adminAuthenticate = require("./middleware/adminAuthentication")
+const blogsRoute = require("./routes/blog");
+const adminRoute = require("./routes/adminRoute");
+const authRoute = require("./routes/auth");
+const adminAuthenticate = require("./middleware/adminAuthentication");
 
+app.use(express.static('./public'));
+app.use(express.json());
 
-app.use(express.json())
+// API routes
+app.use("/api/v1/blog", blogsRoute);
+app.use("/api/v1/admin", adminAuthenticate, adminRoute);
+app.use("/api/v1/auth", authRoute);
 
-app.get("/",(req,res)=> {
-    res.send(`Femi Blog`)
-})
+// If no API routes match, serve the HTML file
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html")); // Use 'path' to construct the file path
+});
 
-app.use("/api/v1/blog",blogsRoute)
-app.use("/api/v1/admin",adminAuthenticate,adminRoute)
-app.use("/api/v1/auth",authRoute)
+const start = async (req, res) => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(3000, () => {
+      console.log(`app is listening on port 3000`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const start = async (req,res)=> {
-    try {
-    await connectDB(process.env.MONGO_URI)
-    app.listen(3000,()=>{
-        console.log(`app is listening on port 3000`)
-    })}
-    catch(error) {
-      console.log(error)
-    }
-}
-
-start()
-
+start();
